095c21400d3465e536c8425cbbec3be8b4a010e4
practicas <andromedalei@outlook.com>
2021-11-19 20:32:42 -0600

    slider e imagenes

--- js/main.js
+++ js/main.js
@@ -26,4 +26,40 @@ window.addEventListener('click', function(e) {
   if (e.target == contModal) {
     contModal.style.display = 'none';
   }
-})
+});
+
+let slider = $('#slider');
+let siguiente = $('#btn__right')
+let anterior = $('#btn__left')
+// mover la ultima imagen al primer lugar
+$('#slider section:last').insertBefore('#slider section:first');
+//mostrar la primera imagen con margin left -100%
+slider.css('margin-left', '-'+100+'%');
+
+function moverD(){
+  slider.animate({marginLeft:'-'+200+'%'}, 700, function(){
+    $('#slider section:first').insertAfter('#slider section:last');
+    slider.css('margin-left', '-'+100+'%');
+  });
+};
+
+function moverI(){
+  slider.animate({marginLeft:0}, 700, function(){
+    $('#slider section:last').insertBefore('#slider section:first');
+    slider.css('margin-left', '-'+100+'%');
+  });
+};
+siguiente.on('click', function(){
+  moverD();
+});
+anterior.on('click', function(){
+  moverI();
+});
+
+function autoplay(){
+  interval = setInterval(function(){
+    moverD()
+  }, 5000);
+}
+autoplay();
+
